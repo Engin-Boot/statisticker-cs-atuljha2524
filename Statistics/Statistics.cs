bee4b4a05c0469ac0dc3defcf71e409fbfd12f09
atuljha2524 <69622477+atuljha2524@users.noreply.github.com>
2020-09-02 13:10:48 +0530

    Update Statistics.cs

--- Statistics/Statistics.cs
+++ Statistics/Statistics.cs
@@ -1,12 +1,29 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 namespace Statistics
 {
-    public class StatsComputer
+    class Stats
     {
-        public Stats CalculateStatistics(List<float> numbers) {
-            //Implement statistics here
+        public double average;
+        public double max;
+        public double min;
+    }
+    class StatsComputer
+    {
+        public Stats CalculateStatistics(List<float> numbers)
+        {
+            Stats obj = new Stats();
+            if (numbers.Count == 0) {
+                obj.max = double.NaN;
+                obj.min = double.NaN;
+                obj.average = double.NaN;
+                return obj;
+            }
+            obj.max = numbers.Max();
+            obj.min = numbers.Min();
+            obj.average = numbers.Average();
+            return obj;
         }
     }
 }
